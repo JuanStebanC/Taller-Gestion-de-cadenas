6a7ea19b22890e564ed2452a0d9fa0f09394a0ea
JuanStebanC <juanstebanleoncardenas@gmail.com>
2023-07-08 21:27:17 -0500

    Creacion del menu
    Creacion del metodo estatico menu, donde se van a ir añadiendo los otros metodos

--- src/edu/uptc/view/Runner.java
+++ src/edu/uptc/view/Runner.java
@@ -1,10 +1,53 @@
 package edu.uptc.view;
 
+import javax.swing.JOptionPane;
+
 public class Runner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
+    private static void mainMenu() {
+        String menu = "<<<<<<<MENU PRINCIPAL>>>>>>>\n"
+        		+ "\n 1. Numero Romano"
+                + "\n 2. Factores Primos"
+                + "\n 3. Borrar Espacios"
+                + "\n 4. Números Ególatras"
+                + "\n 5. Número Mágico"
+                + "\n 6. Fechas"
+                + "\n 7. Salir";
+
+        char option = 0;
+        do{
+            option = JOptionPane.showInputDialog( menu ).charAt(0);
+
+            switch( option ){
+                case '1' :
+                	
+                	break;
+                case '2' :
+                	
+                	break;
+                case '3' :
+                	
+                	break;
+                case '4' :
+                	
+                	break;
+                case '5':
+                	
+                	break;
+                case '6':
+                	
+                	break;
+                case '7':
+                	
+                	break;
+                default:
+                	
+            }
+            
+        }while( true );
+    }
 
 }
